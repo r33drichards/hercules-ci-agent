626bee8860bfce7f7d93dc430043a6b648cc22b6
rw <rw@jjk.is>
2024-03-14 16:55:41 -0700

    use hercules from flake

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, hercules-ci-agent, ... }:
 let
   flakeryDomain = builtins.readFile /metadata/flakery-domain;
 in
@@ -10,17 +10,21 @@ in
     extraUpFlags = [ "--ssh" "--hostname" "hercules" ];
   };
 
+  imports = [
+    hercules-ci-agent.nixosModules.agent-service
+    # ... other imports if you have them
+  ];
 
 
   services.hercules-ci-agent.enable = true;
   services.hercules-ci-agent.concurrentTasks = 4; # Number of jobs to run
 
-    # chown /home/alice/.ssh to alice
+  # chown /home/alice/.ssh to alice
   # set permissions for /home/alice/.ssh/id_ed25519 to 600
   systemd.services.fix-ssh-permissions = {
     description = "Fix SSH permissions";
     after = [ "network-pre.target" ];
-    wants = [ "network-pre.target"];
+    wants = [ "network-pre.target" ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig.Type = "oneshot";
 
--- flake.nix
+++ flake.nix
@@ -4,15 +4,18 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flakery.url = "github:getflakery/flakes";
+    hercules-ci-agent.url = "github:hercules-ci/hercules-ci-agent/stable";
+
   };
 
-  outputs = { self, nixpkgs, flakery }: {
+  outputs = { self, nixpkgs, flakery, hercules-ci-agent }: {
     nixosConfigurations.hello-flakery = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         flakery.nixosModules.flakery
-        ./configuration.nix
+        ./configuration.nix 
       ];
+      specialArgs = { inherit hercules-ci-agent; };
     };
   };
 }
