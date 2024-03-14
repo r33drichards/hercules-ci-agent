d7eb275bf12a579352d14064a48daa76004d0650
rw <rw@jjk.is>
2024-03-14 16:39:04 -0700

    maybe need this

--- configuration.nix
+++ configuration.nix
@@ -11,11 +11,6 @@ in
   };
 
 
-  imports = [
-    ( builtins.fetchTarball "https://github.com/hercules-ci/hercules-ci-agent/archive/stable.tar.gz"
-      + "/module.nix"
-    )
-  ];
 
   services.hercules-ci-agent.enable = true;
   services.hercules-ci-agent.concurrentTasks = 4; # Number of jobs to run
