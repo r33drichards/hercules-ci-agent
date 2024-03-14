a3b15e8dae5f785d70cf67ff993b5db8b7cc3701
rw <rw@jjk.is>
2024-03-14 16:17:30 -0700

    first commit

---
+++ configuration.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+let
+  flakeryDomain = builtins.readFile /metadata/flakery-domain;
+in
+{
+  system.stateVersion = "23.05";
+  services.tailscale = {
+    enable = true;
+    authKeyFile = "/tsauthkey";
+    extraUpFlags = [ "--ssh" "--hostname" "flakery-tutorial" ];
+  };
+
+  
+  imports = [
+    ( builtins.fetchTarball "https://github.com/hercules-ci/hercules-ci-agent/archive/stable.tar.gz"
+      + "/module.nix"
+    )
+  ];
+
+  services.hercules-ci-agent.enable = true;
+  services.hercules-ci-agent.concurrentTasks = 4; # Number of jobs to run
+
+    # chown /home/alice/.ssh to alice
+  # set permissions for /home/alice/.ssh/id_ed25519 to 600
+  systemd.services.fix-ssh-permissions = {
+    description = "Fix SSH permissions";
+    after = [ "network-pre.target" ];
+    wants = [ "network-pre.target"];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.Type = "oneshot";
+
+    script = ''
+      chown -R hercules-ci-agent /var/lib/hercules-ci-agent
+      chmod o-rwx /var/lib/hercules-ci-agent/secrets
+    '';
+  };
+
+
+}
